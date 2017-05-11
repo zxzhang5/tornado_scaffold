25dcba14884a426973e83ac5f68458b34c97c202
howdy <zhixiang.zhang@tyqiu.com>
2017-05-11 18:10:32 +0800

    setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# Tornado Bus Demo
+
+## Installation
+
+```bash
+git clone https://github.com/zxzhang5/tornado_bus_demo.git
+cd tornado_bus_demo
+python setup.py develop
+```
+
+## License
+
+This project is licensed under the MIT License.
---
+++ setup.py
@@ -0,0 +1,46 @@
+import os
+import sys
+__DIR__ = os.path.abspath(os.path.dirname(__file__))
+import codecs
+from setuptools import setup
+import tornado_bus_demo
+
+
+def read(filename):
+    """Read and return `filename` in root dir of project and return string"""
+    return codecs.open(os.path.join(__DIR__, filename), "r").read()
+
+install_requires = [
+    "tornado",
+    "Tornado-JSON"
+]
+long_description = read("README.md")
+
+setup(
+    name="tornado_bus_demo",
+    version=tornado_bus_demo.__version__,
+    url="https://github.com/zxzhang5/tornado_bus_demo",
+    license="MIT License",
+    author="Zhang Zhi Xiang",
+    description="A demo project based on Tornado",
+    long_description=long_description,
+    packages=["tornado_bus_demo"],
+    install_requires=install_requires,
+    data_files=[
+        # Populate this with any files config files etc.
+    ],
+    classifiers=[   # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 1 - Planning",
+        "Environment :: Web Environment",
+        "Framework :: Tornado",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Chinese (Simplified)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ]
+)
